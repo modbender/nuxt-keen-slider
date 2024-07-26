36aae3f287d4f1cec9c3655d557e122054a5360f
modbender <{ID}+{username}@users.noreply.github.com>
2024-07-26 22:03:10 +0530

    chore: lint fix and update cmd

--- playground/components/KeenSlider.vue
+++ playground/components/KeenSlider.vue
@@ -1,11 +1,26 @@
 <template>
-  <div ref="container" class="keen-slider">
-    <div class="keen-slider__slide number-slide1">1</div>
-    <div class="keen-slider__slide number-slide2">2</div>
-    <div class="keen-slider__slide number-slide3">3</div>
-    <div class="keen-slider__slide number-slide4">4</div>
-    <div class="keen-slider__slide number-slide5">5</div>
-    <div class="keen-slider__slide number-slide6">6</div>
+  <div
+    ref="container"
+    class="keen-slider"
+  >
+    <div class="keen-slider__slide number-slide1">
+      1
+    </div>
+    <div class="keen-slider__slide number-slide2">
+      2
+    </div>
+    <div class="keen-slider__slide number-slide3">
+      3
+    </div>
+    <div class="keen-slider__slide number-slide4">
+      4
+    </div>
+    <div class="keen-slider__slide number-slide5">
+      5
+    </div>
+    <div class="keen-slider__slide number-slide6">
+      6
+    </div>
   </div>
 </template>
 
